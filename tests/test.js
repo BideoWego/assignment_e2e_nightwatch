6b33acc7297aa31ee16f5e9968b7f8025133f70e
Bideo Wego <bideowego@gmail.com>
2017-11-28 05:18:16 -0500

    Mostly passing

--- tests/test.js
+++ tests/test.js
@@ -1,3 +1,50 @@
 // Your code here ...
+require('dotenv').config();
 
-module.exports = {};
+
+const Test = {
+  "Home": browser => {
+    browser
+      .url(browser.launchUrl)
+      .waitForElementVisible('body', 1000)
+      .saveScreenshot('screenshots/1.jpg')
+      .end();
+  },
+  "Sign in": browser => {
+    browser
+      .url(browser.launchUrl)
+      .waitForElementVisible('body', 1000)
+      .saveScreenshot('screenshots/2.jpg')
+      .end();
+  },
+  "Login": browser => {
+    browser
+      .url(browser.launchUrl)
+      .waitForElementVisible('body', 1000)
+      .click('#loki-navbar a[href="/sign_in"]')
+      .setValue('#user_email', process.env.EMAIL)
+      .setValue('#user_password', process.env.PASS)
+      .click('#new_user input[type="submit"]')
+      .waitForElementVisible('#angular-loki', 3000)
+      .saveScreenshot('screenshots/3.jpg')
+      .end();
+  },
+  "Dashboard": browser => {
+    browser
+      .url(browser.launchUrl)
+      .waitForElementVisible('body', 1000)
+      .click('#loki-navbar a[href="/sign_in"]')
+      .setValue('#user_email', process.env.EMAIL)
+      .setValue('#user_password', process.env.PASS)
+      .click('#new_user input[type="submit"]')
+      .waitForElementVisible('#angular-loki', 3000)
+      .url(browser.launchUrl + '/dashboard/#/testing-javascript/assignment-viking-code-school-exploration')
+      .saveScreenshot('screenshots/4.jpg')
+      .waitForElementVisible('.lesson-completion', 60000)
+      .click('.lesson-completion .lesson-completion-checkbox img')
+      .end();
+  }
+};
+
+
+module.exports = Test;
